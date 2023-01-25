66ec835505dd6b070aa57670cd9930c67ac4288c
Jepc <joseenrique.pc21@gmail.com>
2023-01-25 10:31:57 -0400

    ADD ListTiles from a static list

--- lib/screens/listview_screen.dart
+++ lib/screens/listview_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
 
+  final options = const['Megaman', 'Metal Gear', 'Super Smash', 'Final Fantasy'];
   const Listview1Screen({super.key});
 
   @override
@@ -11,13 +12,25 @@ class Listview1Screen extends StatelessWidget {
         title: const Text('Listview Tipo 1'),
       ),
       // Lista de widgets
+      // Un ListView en su children aguanta cualquier widget
+      // Flutter por defecto ya tiene un widget que genera las opciones similar a una lista de contactos ListTile
       body: ListView(
-        children: const[
-          Text('Hola mundo'),
-          Text('Hola mundo'),
-          Text('Hola mundo'),
-          Text('Hola mundo'),
-          Text('Hola mundo'),
+        children: [
+          // Text('Hola mundo'),
+          // Lo que esta haciendo es tomando cada uno de los elementos y transformandolos a lo que se retorne del map
+          // Lo que regresa el mas es un iterable algo que puede ser iterado
+          // Lo que hace el operador spread es extraer cada elemento de una lista y mostrar el widget
+          ...options.map(
+            (game) => ListTile(
+              leading: const Icon(Icons.gamepad_outlined),
+              title: Text(game),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+            )
+          ).toList()
+          // ListTile(
+          //   leading: Icon(Icons.account_circle_rounded),
+          //   title: Text('Hola mundo'),
+          // )
         ],
       )
     );
