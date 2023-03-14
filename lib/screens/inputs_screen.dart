16550ff6988ffd0e01634c4ea8e70059d3cdd244
Jepc <joseenrique.pc21@gmail.com>
2023-03-13 22:14:09 -0400

    ADD: centralize the style of the inputs

--- lib/screens/inputs_screen.dart
+++ lib/screens/inputs_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/widgets/widgets.dart';
+
 class InputsScreen extends StatelessWidget {
 
   const InputsScreen({super.key});
@@ -9,7 +11,7 @@ class InputsScreen extends StatelessWidget {
     return Scaffold(
 
       appBar: AppBar(
-        title: Text('Inputs y Forms'),
+        title: const Text('Inputs y Forms'),
       ),
 
       // Cuando se quiera trabajar con formularios aunque sea 1 solo y con un unico input y ese unico input esta en la parte superior de la pantalla tal vez sera una excepcion
@@ -20,70 +22,9 @@ class InputsScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
           child: Column(
-            children: [
-              // Tenemos varios widgets para los inputs(TextFormField(),TextField()) lucen iguales pero
-              //TextFormField() tiene mas iteciones con formularios, se recomienda trabajar cone el TextFormField porque podemos adicionar validaciones automaticas es el mas usado para hacer formularios en flutter
-              //TextField() se utiliza solo cuando se quiere hacer algo sin formulario, cuando no hay un formulario que los agrupe
-              TextFormField(
-                //cuando se construye el widget se pone en el input el foco
-                autofocus: false,
-                initialValue: 'nombre',
-                //textCapitalization se usa para campos donde se quiera capitalizar cada palabra ejemplo nombres y apellidos
-                textCapitalization: TextCapitalization.words,
-                //captura el valor que se esta ingresando en el input
-                // se puede tener aqui algo que este escuchando ese valor y almacenarlo en el estado de la aplicasion global o en estado del statefullwidget o en el estado implementado para manejar el formulario
-                onChanged: ( value ) {
-                  print( 'value: $value');
-                },
-                // imaginemos que en la caja de texto se requiera o que tenga algun tipo de validacion , ejemplo : que sean mas de 5 caracteres, que cumpla con una expresion regular ect.
-                // validator sirve para hacer esas validaciones.
-                validator: (value) {
-                  //la validacion no se esta disparando por que el valor no es nulo
-                  // esto se dispararica cuando tengamos al "TextFormField" envuelto en un formulario y el formulario intente hacer un posteo o intente hacer la verificacion de cada uno de los campos y algun campo no tenga valor hay seria nulo
-                  // no se ve nada por que a este validador necesitamos accionarlo de alguna manera y para eso necesitamos colocar el autovalidateMode
-                  // si regresa un string quiere decir que la validacion no esta pasando, es decir hay algun error
-                  if (value == null) return 'Este campo es requerido';
-                  //se puede evaluar lo que deseen una expresion regular , si se escribio un 2 o un 10 ect lo que se desee evaluar
-                  // pueden hacer lo que deseen con esta funcion de evaluacion que recibimos aqui
-                  // return 'Hola mundo';
-                  // al regresar un null quiere decir que la validacion esta pasando
-                  return value.length < 3 ? 'Minimo de 3 letras' : null;
-                } ,
-                // AutovalidateMode.always siempre se muestra el mensaje
-                // autovalidateMode: AutovalidateMode.always,
-                // la evaluacion no se dispara hasta que la persona haga alguna iteracion con el input
-                // esto tambien se pued colocar en la parte del formulario y el formulario se va a encargar de ver los TextFormField o los diferentes TextFormField no necesariamente solo de texto
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                decoration: const InputDecoration(
-                  // hintText es lo que se muestra en el espacio donde va a escribir el usuario,donde se pone el foco, aparece cuando el usuario pone el foco en ese espacio sino se muestra el labelText
-                  hintText: 'Nombre del usuario',
-                  // se muestra antes del hintText pero se recorre hacia arriba cuando el usuario empieza a iteractuar con el input o espacio
-                  labelText: 'Nombre',
-                  // ayuda al usuario aparece antes de la validacion cuando el usuario no iteractua con el input o caja de texto y despues que el mensaje de la validacion desaparece
-                  helperText: 'Solo letras',
-                  // es util para identificar cuantos caracteres quedan para pasar la validacion
-                  // counterText: ' 3 caracteres'
-                  // es un icono que aparece al inicio de la vaja de texto
-                  prefixIcon: Icon(Icons.verified_user_rounded),
-                  // es un icono que aparece al final de la caja de texto
-                  suffixIcon: Icon(Icons.group_outlined),
-                  // este icono aparece delante de la linea de texte pero la recorre toda la linea , junto con los demas componentes los anteriores no lo recorrian
-                  icon: Icon( Icons.assignment_ind_outlined ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.green
-                    )
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)
-                    )
-                  ),
-                  // no se puede colocar el color por que el input tiene muchos estados
+            children: const [
 
-                ),
-              )
+              CustomInputField( labelText: 'Nombre', hintText: 'Nombre del usuario',)
             ],
           ),
         ),
@@ -93,3 +34,4 @@ class InputsScreen extends StatelessWidget {
     );
   }
 }
+
---
+++ lib/widgets/custom_input_field.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class CustomInputField extends StatelessWidget {
+
+  final String? hintText;
+  final String? labelText;
+  final String? helperText;
+  final IconData? icon;
+  final IconData? suffixIcon;
+
+  const CustomInputField({
+    Key? key,
+    this.hintText,
+    this.labelText,
+    this.helperText,
+    this.icon,
+    this.suffixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Tenemos varios widgets para los inputs(TextFormField(),TextField()) lucen iguales pero
+    //TextFormField() tiene mas iteciones con formularios, se recomienda trabajar cone el TextFormField porque podemos adicionar validaciones automaticas es el mas usado para hacer formularios en flutter
+    //TextField() se utiliza solo cuando se quiere hacer algo sin formulario, cuando no hay un formulario que los agrupe
+    return TextFormField(
+      //cuando se construye el widget se pone en el input el foco
+      autofocus: false,
+      initialValue: 'nombre',
+      //textCapitalization se usa para campos donde se quiera capitalizar cada palabra ejemplo nombres y apellidos
+      textCapitalization: TextCapitalization.words,
+      //captura el valor que se esta ingresando en el input
+      // se puede tener aqui algo que este escuchando ese valor y almacenarlo en el estado de la aplicasion global o en estado del statefullwidget o en el estado implementado para manejar el formulario
+      onChanged: ( value ) {
+        print( 'value: $value');
+      },
+      // imaginemos que en la caja de texto se requiera o que tenga algun tipo de validacion , ejemplo : que sean mas de 5 caracteres, que cumpla con una expresion regular ect.
+      // validator sirve para hacer esas validaciones.
+      validator: (value) {
+        //la validacion no se esta disparando por que el valor no es nulo
+        // esto se dispararica cuando tengamos al "TextFormField" envuelto en un formulario y el formulario intente hacer un posteo o intente hacer la verificacion de cada uno de los campos y algun campo no tenga valor hay seria nulo
+        // no se ve nada por que a este validador necesitamos accionarlo de alguna manera y para eso necesitamos colocar el autovalidateMode
+        // si regresa un string quiere decir que la validacion no esta pasando, es decir hay algun error
+        if (value == null) return 'Este campo es requerido';
+        //se puede evaluar lo que deseen una expresion regular , si se escribio un 2 o un 10 ect lo que se desee evaluar
+        // pueden hacer lo que deseen con esta funcion de evaluacion que recibimos aqui
+        // return 'Hola mundo';
+        // al regresar un null quiere decir que la validacion esta pasando
+        return value.length < 3 ? 'Minimo de 3 letras' : null;
+      } ,
+      // AutovalidateMode.always siempre se muestra el mensaje
+      // autovalidateMode: AutovalidateMode.always,
+      // la evaluacion no se dispara hasta que la persona haga alguna iteracion con el input
+      // esto tambien se pued colocar en la parte del formulario y el formulario se va a encargar de ver los TextFormField o los diferentes TextFormField no necesariamente solo de texto
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      decoration: InputDecoration(
+        // hintText es lo que se muestra en el espacio donde va a escribir el usuario,donde se pone el foco, aparece cuando el usuario pone el foco en ese espacio sino se muestra el labelText
+        // hintText: 'Nombre del usuario',
+        hintText: hintText,
+        // se muestra antes del hintText pero se recorre hacia arriba cuando el usuario empieza a iteractuar con el input o espacio
+        // labelText: 'Nombre',
+        labelText: labelText,
+        // ayuda al usuario aparece antes de la validacion cuando el usuario no iteractua con el input o caja de texto y despues que el mensaje de la validacion desaparece
+        // helperText: 'Solo letras',
+        helperText: helperText,
+        // es util para identificar cuantos caracteres quedan para pasar la validacion
+        // counterText: ' 3 caracteres'
+        // es un icono que aparece al inicio de la vaja de texto
+        // prefixIcon: Icon(Icons.verified_user_rounded),
+        // es un icono que aparece al final de la caja de texto
+        // suffixIcon: Icon(Icons.group_outlined),
+        suffixIcon: suffixIcon == null ? null : Icon(suffixIcon),
+        // este icono aparece delante de la linea de texte pero la recorre toda la linea , junto con los demas componentes los anteriores no lo recorrian
+        // icon: Icon( Icons.assignment_ind_outlined ),
+        icon: icon == null ? null : Icon( icon ),
+        // focusedBorder: OutlineInputBorder(
+        //   borderSide: BorderSide(
+        //     color: Colors.green
+        //   )
+        // ),
+        // border: OutlineInputBorder(
+        //   borderRadius: BorderRadius.only(
+        //     bottomLeft: Radius.circular(10),
+        //     topRight: Radius.circular(10)
+        //   )
+        // ),
+        // no se puede colocar el color por que el input tiene muchos estados
+
+      ),
+    );
+  }
+}
